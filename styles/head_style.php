f16eed99ec1bc75de724eb600901c7fe2519442e
Teddy Walsh <98552735+teddy-walsh@users.noreply.github.com>
2022-11-02 23:32:57 -0400

    Test SQLconn

---
+++ styles/head_style.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">  
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, minimum-scale=1.0">
+        <meta name="authors" content="Joshua Tapp, Teddy Walsh">
+        <meta name="description" content="<?= $description ?>" />  
+        <title><?= $title?></title>
+
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+        <!-- LESS -->
+        <link rel="stylesheet/less" type="text/css" href="styles/styles.less" />
+        <script src="https://cdn.jsdelivr.net/npm/less@4.1.2/dist/less.min.js" ></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+        <!-- CSS Stylesheet -->
+        <!-- <link rel="stylesheet" href="./styles/styles.css"> -->
+
+        <!-- Cloud Hosted -->
+        <!-- https://storage.googleapis.com/cs4640-project-quiz-app/4640-quizlette/index.html -->
+
+        <!-- CS4640 Link -->
+        <!-- https://cs4640.cs.virginia.edu/jct7bm/sprint2 -->
+
+    </head>
+<body>
